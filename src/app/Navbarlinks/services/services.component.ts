96aa742bbe4b5d4b7500b167132a0ed6cb738ed4
abhyiopn <abhy@rapscorp.com>
2023-07-08 23:54:29 +0530

    7/8/23

--- src/app/Navbarlinks/services/services.component.ts
+++ src/app/Navbarlinks/services/services.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AnimationOptions ,} from "ngx-lottie";
 
 @Component({
   selector: 'app-services',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent {
-
+  options: AnimationOptions = {
+    path:"/assets/New-file.json", // download the JSON version of animation in your project directory and add the path to it like ./assets/animations/example.json
+  };
 }
